a9b91a24bbbac9bf7e021d2dd12127453c42f614
chrisruk <chrisrichardsonuk@gmail.com>
2022-11-13 21:11:19 +0000

    Reduce reg size

--- src/matrix.v
+++ src/matrix.v
@@ -24,7 +24,7 @@ module chrisruk_matrix #( parameter MAX_COUNT = 1000 ) (
     reg [11:0] counter1;        // Count where we are in bit pattern
     reg [2:0] shift;            // Amount to left shift letter
 
-    reg [5-1:0] rcount;          // Row number in 8x8 matrix
+    reg [4-1:0] rcount;          // Row number in 8x8 matrix
     reg [3-1:0] rowno;          // Row number in 8x8 matrix
     reg [6-1:0] idx;              // Bit index within colour register
     reg [6-1:0] pidx;           // Bit index within letter, we apply processing on top of this
