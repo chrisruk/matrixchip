456c77475b9de8b9ed6c0ee36c91707f9a260136
chrisruk <chrisrichardsonuk@gmail.com>
2022-11-13 21:18:59 +0000

    Alter font array

--- src/matrix.v
+++ src/matrix.v
@@ -19,12 +19,12 @@ module chrisruk_matrix #( parameter MAX_COUNT = 1000 ) (
     assign io_out[0] = clock_1; // Clock output for LED matrix
     assign io_out[1] = strip_1; // Data output for LED matrix
 
-    reg [0:64-1] fonts [0:2-1]; // Font array
+    reg [0:56-1] fonts [0:2-1]; // Font array
     reg [11:0] counter1;        // Count where we are in bit pattern
     reg [2:0] shift;            // Amount to left shift letter
 
     reg [3-1:0] rowno;          // Row number in 8x8 matrix
-    reg [6-1:0] idx;              // Bit index within colour register
+    reg [6-1:0] idx;            // Bit index within colour register
     reg [6-1:0] pidx;           // Bit index within letter, we apply processing on top of this
                                 // value to create the bitidx value
 
@@ -64,8 +64,8 @@ module chrisruk_matrix #( parameter MAX_COUNT = 1000 ) (
             clock_1 <= 0;
             ledreg1 <= 32'hf0000f00;                 // Number colour
             ledreg2 <= 32'hf0070000;                 // Background colour
-            fonts[0] <= 64'h7c_c6_ce_de_f6_e6_7c_00; // 0
-            fonts[1] <= 64'h30_70_30_30_30_30_fc_00; // 1
+            fonts[0] <= 56'h7c_c6_ce_de_f6_e6_7c; // 0
+            fonts[1] <= 56'h30_70_30_30_30_30_fc; // 1
             digit1_cache <= 0;
             digit2_cache <= digit1;
             first = 1;
@@ -75,14 +75,14 @@ module chrisruk_matrix #( parameter MAX_COUNT = 1000 ) (
                 if (counter1 < 32) begin
                     strip_1 = 0;
                     if(!first) begin
-                        display = {fonts[digit1_cache][56:63] << shift, fonts[digit1_cache][48:55] << shift,
+                        display = {8'b0, fonts[digit1_cache][48:55] << shift,
                                    fonts[digit1_cache][40:47] << shift, fonts[digit1_cache][32:39] << shift,
                                    fonts[digit1_cache][24:31] << shift, fonts[digit1_cache][16:23] << shift,
                                    fonts[digit1_cache][8:15]  << shift, fonts[digit1_cache][0:7]   << shift};
                     end else begin
                         display = 0;
                     end 
-                    display = display | {fonts[digit2_cache][56:63] >> 8 - shift, fonts[digit2_cache][48:55] >> 8 - shift,
+                    display = display | {8'b0, fonts[digit2_cache][48:55] >> 8 - shift,
                                fonts[digit2_cache][40:47] >> 8 - shift, fonts[digit2_cache][32:39] >> 8 - shift,
                                fonts[digit2_cache][24:31] >> 8 - shift, fonts[digit2_cache][16:23] >> 8 - shift,
                                fonts[digit2_cache][8:15]  >> 8 - shift, fonts[digit2_cache][0:7]   >> 8 - shift};
